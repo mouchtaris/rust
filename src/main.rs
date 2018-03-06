8556f0fb6c016610562cdc76b5cad6f00a531b59
Nikos Mouchtaris <Nikos Mouchtaris mouchtaris@gmail.com>
2018-03-06 07:27:07 +0000

    Iter begin

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,64 @@ use ::fmap::FMap;
 use ::flat_map::FlatMap;
 use ::pure_point::PurePoint;
 
+
+pub trait Iter
+    where Self: Sized
+{
+    type Element;
+    fn next(&self) -> Optian<(Self, Self::Element)>;
+}
+
+
+pub struct ABC
+{
+    a: i32,
+    b: i32,
+    c: i32
+}
+mod ABCIter {
+    use ::ABC;
+    use ::option::Optian;
+    use ::fmap::FMap;
+    #[derive(Clone,Copy)]
+    enum State { a, b, c, e }
+    pub struct Iter<'a> { src: &'a ABC, state: State }
+    impl<'a> ::std::clone::Clone for Iter<'a>
+    {
+        fn clone(&self) -> Self
+        {
+            Iter { .. *self }
+        }
+    }
+    impl<'a> ::std::marker::Copy for Iter<'a>
+    {
+    }
+    impl <'a> ::Iter for Iter<'a>
+    {
+        type Element = i32;
+        fn next(&self) -> Optian<(Iter<'a>, i32)>
+        {
+            let (nextState, maybeValue) = match self.state {
+                State::a => (State::b, Optian::Some(self.src.a)),
+                State::b => (State::c, Optian::Some(self.src.b)),
+                State::c => (State::e, Optian::Some(self.src.c)),
+                State::e => (State::e, Optian::None)
+            };
+            maybeValue.fmap(|v| {
+                let nextSelf = Iter { src: self.src, state: nextState };
+                (nextSelf, v)
+            })
+        }
+    }
+    pub fn create(src: &ABC) -> Iter { Iter { src, state: State::a } }
+}
+
+impl ABC
+{
+    fn iter<'a>(&'a self) -> ABCIter::Iter<'a> { ABCIter::create(self) }
+}
+
+
 fn main() {
     let a = Optian::Some(12);
     let b = Optian::None::<i32>;
